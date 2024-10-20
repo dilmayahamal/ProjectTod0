7fb745f379c3c84bd68bcb9bb17a12157ecaaa1d
Dilmaya Hamal <dilmayahamal09@gmail.com>
2024-10-20 23:49:09 +0545

    ws socket.io start

--- server.ts
+++ server.ts
@@ -1,14 +1,23 @@
 import app from './src/app'
 import { envConfig } from './src/config/config'
 import connectToDb from './src/config/db'
+import {Server} from 'socket.io'
 
 
 function startServer(){
      connectToDb()
     const port = envConfig.port || 4000
-    app.listen(port,()=>{
+    const server = app.listen(port,()=>{
         console.log(`Server has started at port[${port}]`)
     })
+    const io = new Server(server)
+    io.on("connection",(socket)=>{
+       socket.emit("message",{
+        name : "Dilmaya Hamal"
+       })
+        console.log("Someone connected (client)")
+    })
+    
 }
 
 startServer()
