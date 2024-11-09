0b0a1ef438b1a823ca4c293b81a79ca45b1c8dee
Dilmaya Hamal <dilmayahamal09@gmail.com>
2024-11-09 20:58:27 +0545

    add Home UI

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import path from 'path'
 const app = express()
-
-
 import("../src/todo/todoController")
 
-export default app 
+app.set("views", path.join(__dirname,'views'))
+app.set("view engine", "ejs")
+app.get("/",(req:Request,res:Response)=>{
+    res.render("home.ejs")
+})
 
+export default app 
